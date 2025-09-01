ce412b914032547427674c84ba7d020a847bbb79
Sebastian <Sebastian@DESKTOP-Q9NF5AT>
2025-08-31 20:26:15 -0400

    Added code to accept and display the first name.

--- Chapter2examples.cpp
+++ Chapter2examples.cpp
@@ -14,12 +14,16 @@ int main()
     char firstInitial;
     char middleInitial;
     char lastInitial;
+    std::string firstName;
     std::cout<<"Please enter the initial of your first name:";
     std::cin>>firstInitial;
     std::cout<<"Please enter the initial of your middle name:";
     std::cin>>middleInitial;
     std::cout<<"Please enter the initial of your last name:";
     std::cin>>lastInitial;
-    std::cout << "Your initials are:" <<firstInitial <<middleInitial <<lastInitial;
+    std::cout << "Your initials are:" << firstInitial << middleInitial << lastInitial << std::endl;
+    std::cout << "Please enter your first name:";
+    std::cin >> firstName;
+    std::cout << "Your first name is:" << firstName;
 }
 
